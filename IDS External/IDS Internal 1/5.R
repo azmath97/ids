f1d0e574a05d2670635c348d9eb8f053eb32124c
Shaik Azmath <ugs23063_aids.shaik@cbit.org.in>
2025-12-09 19:09:00 +0530

    Update 5.R

--- IDS External/IDS Internal 1/5.R
+++ IDS External/IDS Internal 1/5.R
@@ -1,13 +1,11 @@
-numbers <- scan("C:/Users/Abhi/Downloads/input_numbers.txt")
-
-total_sum <- sum(numbers)
-total_mean <- mean(numbers)
-
-results <- c(paste("Sum =", total_sum), paste("Mean =", total_mean))
-
-writeLines(results, "C:/Users/Abhi/Downloads/output_results.txt")
+numbers <- scan("input.txt") 
+s <- sum(numbers) 
+m <- mean(numbers) 
+results <- c(s, m) 
+write(results, file = "output.txt") 
 
 x <- c(1,2,3,4,5)
 y <- c(2,4,6,3,1)
 
 plot(x, y, xlab="X-axis", ylab="Y-axis", main="Scatter Plot")
+
