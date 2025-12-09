ee7a2b9ca02a382506abc2d75f8fd9c1de2e2030
Shaik Azmath <ugs23063_aids.shaik@cbit.org.in>
2025-12-09 19:09:32 +0530

    Update 8.R

--- IDS External/IDS Internal 1/8.R
+++ IDS External/IDS Internal 1/8.R
@@ -1,5 +1,5 @@
 library(reshape2)
-
+data("airquality")
 m <- melt(airquality, id=c("Month","Day"))                 # melt
 c <- dcast(m, Month ~ variable, mean, na.rm=TRUE)          # cast with mean
 
@@ -13,4 +13,5 @@ y <- c(6, 7, 8, 9, 10)
 t_result <- t.test(x, y)
 
 # Extract p-value
+
 t_result$p.value
